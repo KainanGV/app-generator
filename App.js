ee57ce714486f93c85e6ac89e079ade6832a923f
Kainan Gabriel <kainangabriel2019@gmail.com>
2021-03-11 22:20:43 -0300

    layout da pagina

--- App.js
+++ App.js
@@ -1,21 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import Slider from '@react-native-community/slider';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={require('./src/assets/logo.png')}
+        style={styles.logo}
+      />
+
+      <Text style={styles.title}>12 Caracteres</Text>
+
+      <Text style={styles.title}></Text>
+
+      <View style={styles.area}>
+        <Slider
+          styles={{ height: 50 }}
+          minimumValue={5}
+          maximumValue={15}
+          minimumTrackTintColor="#ff0000"
+          maximumTrackTintColor="#000"
+        />
+      </View>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Gerar senha</Text>
+      </TouchableOpacity>
+
+      <View style={styles.area}>
+        <Text style={styles.password}>4234242423</Text>
+      </View>
+
+
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#f3f3ff'
   },
-});
+
+  logo: {
+    marginBottom: 60
+  },
+
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+
+  area: {
+    marginTop: 15,
+    marginBottom: 15,
+    backgroundColor: '#fff',
+    width: '90%',
+    borderRadius: 7
+  },
+
+  button: {
+    backgroundColor: 'FFA200',
+    width: '80%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 7,
+    marginBottom: 25
+  },
+
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold'
+  },
+
+  password: {
+    padding: 10,
+    textAlign: 'center',
+    fontSize: 20
+  }
+
+
+})
